5849f892c8741812c7199c13888dcba4abc15c1d
Dongyan Shao <66828533+shaodongyan@users.noreply.github.com>
2024-02-04 15:18:23 +0800

    Add files via upload

--- amr_view.R
+++ amr_view.R
@@ -13,7 +13,7 @@ command <- args[1]
 
 if (command == "plot") {
   # 如果需要的library没有的话，先下载
-  packages <- c("reshape2","tidyverse","jsonlite","readr","RPostgreSQL","pheatmap","RColorBrewer")
+  packages <- c("reshape2","tidyverse","jsonlite","readr","RPostgreSQL","ComplexHeatmap","RColorBrewer")
   new_packages <- packages[!(packages %in% installed.packages()[, "Package"])]
   if (length(new_packages)) {
     install.packages(new_packages, repos = "https://mirrors.tuna.tsinghua.edu.cn/CRAN/")
@@ -26,7 +26,7 @@ if (command == "plot") {
   library(reshape2)
   library(RColorBrewer)
   library(RPostgreSQL)
-  library(pheatmap)
+  library(ComplexHeatmap)
   #命令行选项
   option_list <- list(
     make_option(c("--config"), type = "character", default = NULL, help = "Path to JSON config file", metavar = "character")
@@ -59,16 +59,16 @@ if (command == "plot") {
         #切换文件夹
     setwd(config$others$path)
     #数据库操作
-    host <- "192.168.1.225"
+    host <- "localhost"
     port <- 5432
     dbname <- "bacterial"
     user <- "bacterial"
-    password <- "VastbaseG200"
+    password <- "VastbaseG100"
     con <- dbConnect(PostgreSQL(), host = host, port = port, dbname = dbname, user = user, password = password)
     sql=paste0("UPDATE sys_bacterial_analysis SET view_status = '1',view_png = '",config$others$pngpath,"',view_pdf='",config$others$pdfpath,"' WHERE id = '",config$others$ID,"';")
     sql2=paste0("UPDATE sys_bacterial_analysis SET view_status = '2' WHERE id = '",config$others$ID,"';")
-    print(sql)
-    print(sql2)
+    #print(sql)
+    #print(sql2)
     data_file_path=paste0(data_file_path,"/summary.tab")
     # 检查输入文件是否存在
       if (!file.exists(data_file_path)) {
@@ -106,47 +106,56 @@ if (command == "plot") {
       data2=t(data2)##翻转回去
     }
     #判断是否显示数字（如果显示数字，将NA替换为空，并显示coverage）
-    if (config$plot_settings$display_numbers) {
-      data4 <- data2
-      data4[is.na(data4)] <- ""
-    }else{
-      data4 <- FALSE
-    }
-    pheatmap(data2,
-             cluster_cols = config$plot_settings$cluster_cols,#列聚类
-             cluster_rows = config$plot_settings$cluster_rows,#行聚类
-             cellheight = config$plot_settings$cellheight,#高度
-             cellwidth = config$plot_settings$cellwidth,#宽度
-             display_numbers = data4,#展示数值
-             border_color = config$plot_settings$border_color,#边框颜色
-             number_color = config$plot_settings$number_color,#颜色
-             main = config$general$title,#标题
-             na_col = config$plot_settings$nacolor,
-             fontsize=config$plot_settings$fontsize,#字体大小
-             color = colorRampPalette(rev(brewer.pal(n = 7, name =config$plot_settings$color_type)))(100),#颜色
-             width = config$plot_settings$width,#图片宽度
-             height = config$plot_settings$height,#图片高度
-             angle_col=config$plot_settings$angle_col,#字体倾斜角度
-             filename = config$others$filename1)    
-    pheatmap(data2,
-             cluster_cols = config$plot_settings$cluster_cols,#列聚类
-             cluster_rows = config$plot_settings$cluster_rows,#行聚类
-             cellheight = config$plot_settings$cellheight,#高度
-             cellwidth = config$plot_settings$cellwidth,#宽度
-             display_numbers = data4,#展示数值
-             border_color = config$plot_settings$border_color,#边框颜色
-             number_color = config$plot_settings$number_color,#颜色
-             main = config$general$title,#标题
-             na_col = config$plot_settings$nacolor,
-             fontsize=config$plot_settings$fontsize,#字体大小
-             color = colorRampPalette(rev(brewer.pal(n = 7, name =config$plot_settings$color_type)))(100),#颜色
-             width = config$plot_settings$width,#图片宽度
-             height = config$plot_settings$height,#图片高度
-             angle_col=config$plot_settings$angle_col,#字体倾斜角度
-             filename = config$others$filename2)                
-   
-    dbSendQuery(con, sql) 
+    data2=as.matrix(data2)
+    png(filename = config$others$filename1,type="cairo",res = 300,width =config$plot_settings$width*500,height = config$plot_settings$height*500)
+    ht= ComplexHeatmap::Heatmap(data2,
+                                cluster_columns = F,#列聚类
+                                cluster_rows = F,#行聚类
+                                height = nrow(data2)*unit(config$plot_settings$cellheight, "pt") ,#高度
+                                width = ncol(data2)*unit(config$plot_settings$cellwidth, "pt"),
+                                cell_fun = function(j, i, x, y, width, height, fill) {
+                                  if(config$plot_settings$display_numbers)
+                                    if(!is.na(data2[i,j]))
+                                      grid.text(sprintf("%.1f", data2[i, j]), x, y, gp = gpar(
+                                        fontsize = config$plot_settings$fontsize,#字体大小
+                                        col=config$plot_settings$number_color))    #颜色))
+                                },#展示数值
+                                rect_gp = gpar(col = config$plot_settings$border_color),#边框颜色                                
+                                column_title = config$general$title,#标题
+                                na_col = config$plot_settings$nacolor,
+                                color = rev(brewer.pal(n = 7, name = config$plot_settings$color_type)),#颜色                                
+                                column_names_rot=config$plot_settings$angle_col,
+                                row_names_gp = gpar(fontsize = config$plot_settings$fontsize),
+                                column_names_gp = gpar(fontsize = config$plot_settings$fontsize),
+                                heatmap_legend_param = list(title = "Identification")
+    )
+    draw(ht)
+    dev.off()
     
+    pdf(file = config$others$filename2,width =config$plot_settings$width,height = config$plot_settings$height )
+    ComplexHeatmap::Heatmap(data2,
+                            cluster_columns = F,#列聚类
+                            cluster_rows = F,#行聚类
+                            height = nrow(data2)*unit(config$plot_settings$cellheight, "pt") ,#高度
+                            width = ncol(data2)*unit(config$plot_settings$cellwidth, "pt"),
+                            cell_fun = function(j, i, x, y, width, height, fill) {
+                              if(config$plot_settings$display_numbers)
+                                if(!is.na(data2[i,j]))
+                                  grid.text(sprintf("%.1f", data2[i, j]), x, y, gp = gpar(
+                                  fontsize = config$plot_settings$fontsize,#字体大小
+                                  col=config$plot_settings$number_color))    #颜色))
+                            },#展示数值
+                            rect_gp = gpar(col = config$plot_settings$border_color),#边框颜色                            
+                            column_title = config$general$title,#标题
+                            na_col = config$plot_settings$nacolor,
+                            color = rev(brewer.pal(n = 7, name = config$plot_settings$color_type)),#颜色                            
+                            column_names_rot=config$plot_settings$angle_col,
+                            row_names_gp = gpar(fontsize = config$plot_settings$fontsize),
+                            column_names_gp = gpar(fontsize = config$plot_settings$fontsize),
+                            heatmap_legend_param = list(title = "Identification")
+    )
+    dev.off()
+    dbSendQuery(con, sql)
   }
   plot(json_file_path)
 }
